f6367632806ee8ed317041a8c5236ae2a26c37f8
trancuong <trancuong4896@gmail.com>
2025-09-04 20:43:14 +0700

    successed bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,4 +1,4 @@
-
+import crypto from "crypto";
 export type Block = {
   index: number;
   timestamp: string;
@@ -9,5 +9,10 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const value = 
+    block.index +
+    block.timestamp +
+    JSON.stringify(block.transactions) +
+    block.previous_hash;
+    return crypto.createHash('sha256').update(value).digest('hex') === block.current_hash;
 }
